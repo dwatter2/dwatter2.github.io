75fd70c8540f33632d47307e1348132c4b9df7f9
dwatter2 <dylan.watters001@gmail.com>
2024-02-29 17:31:11 -0500

    fixed project overflow

--- public/javascript/slideshow.js
+++ public/javascript/slideshow.js
@@ -1,5 +1,13 @@
 let slideNum = 0;
-let slideGroup = 2;
+let slideGroup = 1;
+const totalSlidesOverall = document.getElementsByClassName("project-card");
+//hide slides
+for(let i = 0; i < totalSlidesOverall.length; i++) {
+    totalSlidesOverall[i].hidden = true;
+}
+
+console.log(totalSlidesOverall);
+
 displaySlide(slideNum);
 
 function slideBack() {
@@ -13,19 +21,25 @@ function slideNext() {
 }
 
 function displaySlide(num) {
-    let totalSlides = document.getElementById("p" + slideGroup).getElementsByTagName("img");
+    let totalSlidesCurrent = document.getElementById("p" + slideGroup).getElementsByTagName("img");
+    totalSlidesOverall[slideGroup-1].hidden = false;
+    console.log("group: " + slideGroup);
 
-    if(totalSlides.length > 1 && totalSlides != null) {
-        if(totalSlides.length-1 < num)
+    if(totalSlidesCurrent.length > 1 && totalSlidesCurrent != null) {
+        //overflow
+        if(totalSlidesCurrent.length-1 < num)
             slideNum = 0;
+        //underflow?
         if(num < 0)
-            slideNum = totalSlides.length-1;
+            slideNum = totalSlidesCurrent.length-1;
 
-        for(let i = 0; i < totalSlides.length; i++) {
-            totalSlides[i].style.display = "none";
+        //Set all slides back to display: none
+        for(let i = 0; i < totalSlidesCurrent.length; i++) {
+            totalSlidesCurrent[i].style.display = "none";
         }
-        totalSlides[slideNum].style.display = "block";
-        document.getElementById("p" + slideGroup).querySelector(".slideshow-current").innerHTML = (slideNum + 1).toString() + "/" + totalSlides.length.toString();
+        //Set current to visible and set display number
+        totalSlidesCurrent[slideNum].style.display = "block";
+        document.getElementById("p" + slideGroup).querySelector(".slideshow-current").innerHTML = (slideNum + 1).toString() + "/" + totalSlidesCurrent.length.toString();
     }
 }
 
